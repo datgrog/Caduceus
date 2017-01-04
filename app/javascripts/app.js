7ad8f3206183e0d3cce3355dddbaa6df5e2f13fc
Antoine <antoine.guidis@live.fr>
2017-01-04 20:30:23 +0100

    first commit

---
+++ app/javascripts/app.js
@@ -0,0 +1,86 @@
+const greeter = Greeter.deployed();
+var accounts;
+var account;
+
+console.log('hellow');
+console.log(greeter);
+
+function getGreeter() {
+    let greeeting_element = document.getElementById("greeting");
+    greeter.greet().then((res) => {
+        console.log('coucou : ' + res);
+        greeeting_element.innerHTML = res;
+    }).catch(function(e) {
+        console.log(e);
+        setStatus("Error setGreeter(); see log.");
+    });
+};
+
+function setGreeter() {
+    let newString = document.getElementById("greeting_content").value;
+    // setStatus("Initiating transaction... (please wait)");
+
+    greeter.setGreeting(newString, {from: account}).then((res) => {
+        console.log('success!');
+        // setStatus("Transaction complete!");
+    }).catch(function(e) {
+        console.log(e);
+        setStatus("Error setGreeter(); see log.");
+    });
+};
+
+
+function setStatus(message) {
+    var status = document.getElementById("status");
+    status.innerHTML = message;
+};
+
+/*function refreshBalance() {
+    var meta = MetaCoin.deployed();
+
+    meta.getBalance.call(account, {from: account})
+        .then(function(value) {
+            var balance_element = document.getElementById("balance");
+            balance_element.innerHTML = value.valueOf();
+        })
+        .catch(function(e) {
+            console.log(e);
+            setStatus("Error getting balance; see log.");
+        });
+};
+
+function sendCoin() {
+    var meta = MetaCoin.deployed();
+
+    var amount = parseInt(document.getElementById("amount").value);
+    var receiver = document.getElementById("receiver").value;
+
+    setStatus("Initiating transaction... (please wait)");
+
+    meta.sendCoin(receiver, amount, {from: account}).then(function() {
+        setStatus("Transaction complete!");
+        refreshBalance();
+    }).catch(function(e) {
+        console.log(e);
+        setStatus("Error sending coin; see log.");
+    });
+};*/
+
+window.onload = function() {
+    web3.eth.getAccounts(function(err, accs) {
+        if (err != null) {
+            alert("There was an error fetching your accounts.");
+            return;
+        }
+
+        if (accs.length == 0) {
+          alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
+          return;
+        }
+
+        accounts = accs;
+        account = accounts[0];
+
+        // refreshBalance();
+  });
+}
---
+++ contracts/Caduceus.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.4.7;
+
+contract Caduceus {
+
+/* uint16[24] kidneyData;
+ * ----------------------------------------------------------------------------------------------------------------------------------------
+ * |                   DECIMAL VALUE                     |      FLOAT VALUE     |               NOMINAL VALUE                             |
+ * ----------------------------------------------------------------------------------------------------------------------------------------
+ * |  0  | 1  | 2  | 3  | 4  | 5   | 6  | 7   | 8   | 9  | 10 | 11 | 12  | 13   | 14  | 15 | 16  | 17 | 18  | 19 | 20  | 21    | 22 | 23  |
+ * | age | bp | sg | al | su | bgr | bu | sod | pcv | wc | rc | sc | pot | hemo | rbc | pc | pcc | ba | htn | dm | cad | appet | pe | ane |
+ * ----------------------------------------------------------------------------------------------------------------------------------------
+ *
+ * /!\ uint16 range [0;65535] ; 65535 <=> 'NA' ;
+ *
+ * DECIMAL VALUE :
+ *      - Specific Gravity (1.005,1.010,1.015,1.020,1.025) <=> (5,10,15,20,25)
+ * FLOAT   VALUE :
+ *      - for now all filds are 'NA'
+ * NOMINAL VALUE :
+ *      - (0:1) <=> (false;true)
+ * TEST :
+ *                   | age | bp | sg | al | su | bgr | bu | sod | pcv | wc | rc | sc | pot | hemo | rbc | pc | pcc | ba | htn | dm | cad | appet | pe | ane |
+ * caduceus.addPatient([60,  80,  25,   0,   0,   131,  10, 146,  41, 10700, 99,  99,  99,    99,    1,   1,   0,    0,   0,    0,   0,     1,     0,   0]);
+ * [60,80,25,0,0,131,10,146,41,10700,99,99,99,99,1,1,0,0,0,0,0,1,0,0]
+ */
+
+    struct Patient {
+        address     contact;
+        uint16[24]  kidneyData;
+    }
+
+    uint nextPatientId;
+    mapping (uint256 => Patient) private patients;
+
+    function Caduceus() {
+        // constructor
+    }
+
+    function getNextPatientId() constant returns (uint256) {
+        return nextPatientId;
+    }
+
+    function addPatient(uint16[24] patientData) returns (uint id) {
+        var patient = patients[nextPatientId];
+
+        patient.contact = tx.origin;
+        patient.kidneyData = patientData;
+
+        nextPatientId++;
+        id = nextPatientId;
+    }
+
+    function getKidneyDataFromPatientId(uint256 patientId) constant returns (uint16[24] kidneyData) {
+        kidneyData = patients[patientId].kidneyData;
+    }
+
+    function getContactFromPatientId(uint256 patientId) constant returns (address contact) {
+        contact = patients[patientId].contact;
+    }
+}
