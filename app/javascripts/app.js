48e70dc6b0e0664062756b26290ff293436668ed
Antoine <antoine.guidis@live.fr>
2017-01-06 22:16:04 +0100

    fetch kidneyData + fill table properly OK

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,72 +1,88 @@
 const caduceus = Caduceus.deployed();
+const NB_TABLE = 3;
+var DOMtable = [];
 var accounts;
 var account;
 
-console.log('hellow');
-console.log(caduceus);
+function updateTable(kidneyData) {
+    // kidneyData [60,80,25,0,0,131,10,146,41,10700,510,50,500,1450,1,1,0,0,0,0,0,1,0,0]);
+    let kidneyDataIdx = 0;
 
-/*function getGreeter() {
-    let greeeting_element = document.getElementById("greeting");
-    greeter.greet().then((res) => {
-        console.log('coucou : ' + res);
-        greeeting_element.innerHTML = res;
-    }).catch(function(e) {
-        console.log(e);
-        setStatus("Error setGreeter(); see log.");
+    for(let tableIndex=0; tableIndex < NB_TABLE; tableIndex++) {
+        for(let dataIdx=0; dataIdx < DOMtable[tableIndex].childElementCount; dataIdx++) {
+            // /!\ STRING
+            const currentKidneyDataValue = kidneyData[kidneyDataIdx].toString();
+
+            if(currentKidneyDataValue === '65535') {
+                DOMtable[tableIndex].children[dataIdx].innerHTML = 'NA';
+            } else {
+                switch(kidneyDataIdx) {
+                    case 2:
+                        DOMtable[tableIndex].children[dataIdx].innerHTML = '1.0'+currentKidneyDataValue;
+                        break;
+                    case 10:
+                    case 11:
+                    case 12:
+                    case 13:
+                        // check bignumber js methods https://github.com/MikeMcl/bignumber.js/
+                        // 10 to 13 are float value;
+                        DOMtable[tableIndex].children[dataIdx].innerHTML = kidneyData[kidneyDataIdx].div(100).toString();
+                        break;
+                    case 14:
+                    case 15:
+                        // (normal,abnormal) -> rbc Red Blood Cells || pc  Pus Cell
+                        DOMtable[tableIndex].children[dataIdx].innerHTML = (currentKidneyDataValue === '1') ? 'normal' : 'abnormal';
+                        break;
+                    case 16 :
+                    case 17:
+                        // (present,notpresent) -> pcc Pus Cell clumps || ba  Bacteria
+                        DOMtable[tableIndex].children[dataIdx].innerHTML = (currentKidneyDataValue === '1') ? 'present' : 'notpresent';
+                        break;
+                    case 18:
+                    case 19:
+                    case 20:
+                    case 22:
+                    case 23:
+                        // (yes,no) -> htn Hypertension dm  || Diabetes Mellitus cad || cad Coronary Artery Disease
+                        // (yes,no) -> pe  Pedal Edema      || ane Anemia
+                        DOMtable[tableIndex].children[dataIdx].innerHTML = (currentKidneyDataValue === '1') ? 'yes' : 'no';
+                        break;
+                    case 21:
+                        // (good,poor) -> appet Appetite
+                        DOMtable[tableIndex].children[dataIdx].innerHTML = (currentKidneyDataValue === '1') ? 'good' : 'poor';
+                        break;
+                    default:
+                        DOMtable[tableIndex].children[dataIdx].innerHTML = currentKidneyDataValue;
+                }
+            }
+            kidneyDataIdx++;
+        }
+    }
+}
+
+function getMyKidneyData() {
+    return new Promise(function(resolve) {
+
+        caduceus.getKidneyDataFromPatientId(2).then(function (kidneyData) {
+            resolve(kidneyData);
+        }).catch(function(e) {
+            console.log(e);
+            setStatus("Error getMyKidneyData(); see log.");
+        });
     });
+
 };
-
-function setGreeter() {
-    let newString = document.getElementById("greeting_content").value;
-    // setStatus("Initiating transaction... (please wait)");
-
-    greeter.setGreeting(newString, {from: account}).then((res) => {
-        console.log('success!');
-        // setStatus("Transaction complete!");
-    }).catch(function(e) {
-        console.log(e);
-        setStatus("Error setGreeter(); see log.");
-    });
-};*/
-
 
 function setStatus(message) {
     var status = document.getElementById("status");
     status.innerHTML = message;
 };
 
-/*function refreshBalance() {
-    var meta = MetaCoin.deployed();
-
-    meta.getBalance.call(account, {from: account})
-        .then(function(value) {
-            var balance_element = document.getElementById("balance");
-            balance_element.innerHTML = value.valueOf();
-        })
-        .catch(function(e) {
-            console.log(e);
-            setStatus("Error getting balance; see log.");
-        });
-};
-
-function sendCoin() {
-    var meta = MetaCoin.deployed();
-
-    var amount = parseInt(document.getElementById("amount").value);
-    var receiver = document.getElementById("receiver").value;
-
-    setStatus("Initiating transaction... (please wait)");
-
-    meta.sendCoin(receiver, amount, {from: account}).then(function() {
-        setStatus("Transaction complete!");
-        refreshBalance();
-    }).catch(function(e) {
-        console.log(e);
-        setStatus("Error sending coin; see log.");
-    });
-};*/
-
 window.onload = function() {
+
+    DOMtable.push(document.getElementById('decimal'));
+    DOMtable.push(document.getElementById('float'));
+    DOMtable.push(document.getElementById('nominal'));
 
     web3.eth.getAccounts(function(err, accs) {
         console.log("test");
@@ -83,7 +99,17 @@ window.onload = function() {
 
         accounts = accs;
         account = accounts[0];
-
-        // refreshBalance();
+        
+        getMyKidneyData()
+            .then(updateTable)
+            .then(function() {
+                for(let tableIndex=0; tableIndex < NB_TABLE; tableIndex++) {
+                    // make class 'hide' disappear
+                    DOMtable[tableIndex].parentElement.parentElement.className = "table";
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
   });
 }
